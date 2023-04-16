c2f71ee1ae7db33b83678c1b1808618e23d6cb8e
epiphany07 <96184224+epiphany07@users.noreply.github.com>
2023-04-16 11:20:22 +0530

    Time: 35 ms (76.92%), Space: 13.8 MB (100.00%) - LeetHub

---
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
@@ -0,0 +1,49 @@
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        # word=word.replace("abc","")
+        # print(word)
+        c,i=0,0
+        # l=len(word)
+        while i<len(word):
+            # print(i)
+            if i>=len(word) or word[i]!='a':
+                c+=1
+            else:
+                i+=1
+                # print(i)
+            if i>=len(word) or word[i]!='b':
+                # print(c)
+                c+=1
+            else:
+                i+=1
+            if i>=len(word) or word[i]!='c':
+                c+=1
+            else:
+                i+=1
+        return c
+            
+#         while "ab" in word or "bc" in word or "ac" in word:
+            
+#             if "ab" in word:
+#                 c+=1
+#                 word=word.replace("ab","",1)
+#             if "bc" in word:
+#                 c+=1
+#                 word=word.replace("bc","",1)
+#             if "ac" in word:
+#                 c+=1
+#                 word=word.replace("ac","",1)
+#         while "a" in word or "b" in word or "c" in word:
+            
+#             if "a" in word:
+#                 c+=2
+#                 word=word.replace("a","",1)
+#             if "b" in word:
+#                 c+=2
+#                 word=word.replace("b","",1)
+#             if "c" in word:
+#                 c+=2
+#                 word=word.replace("c","",1)
+#         if word=="":
+#             return c
+        
